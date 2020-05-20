a86421b08e7c447a640f8416b2de90faaa6f3f2f
svn.tmr <svn.tmr@bk.ru>
2020-05-20 09:54:12 +0300

    Completed fourth assignment
    Added table with requests displaying

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-echo 'Hello, World from Docker!';
-
 $getIp = static function () {
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
         return $_SERVER['HTTP_CLIENT_IP'];
@@ -21,14 +19,56 @@ $getTimestamp = static function () {
     return date('Y-d-m H:i:s +v');
 };
 
-$putRequestLine = static function () use ($getIp, $getHostname, $getPath, $getTimestamp) {
+$createConnection = static function () {
     $password = getenv('POSTGRES_PASSWORD');
     $user = getenv('POSTGRES_USER');
     $host = getenv('POSTGRES_HOST');
     $db = getenv('POSTGRES_DB');
     $connectionString = ("host={$host} dbname={$db} user={$user} password={$password}");
     $pgConnection = pg_connect($connectionString);
+    return $pgConnection;
+};
+
+$pgConnection = $createConnection();
+
+$putRequestLine = static function () use ($getIp, $getHostname, $getPath, $getTimestamp, $pgConnection) {
     $query = "INSERT INTO requests (ip, path, host, requested_at) VALUES ('{$getIp()}', '{$getPath()}', '{$getHostname()}', '{$getTimestamp()}')";
     pg_query($pgConnection, $query);
 };
 $putRequestLine();
+
+$getRequests = static function () use ($pgConnection) {
+    $query = 'SELECT id, ip, path, host, requested_at FROM requests ORDER BY id DESC LIMIT 25';
+    $queryResource = pg_query($pgConnection, $query);
+    return pg_fetch_all($queryResource);
+};
+
+$requests = $getRequests();
+?>
+
+<html lang="en">
+    <body>
+        <h1>Hello, World from Docker!</h1>
+        <h2>The requests</h2>
+        <table style="width: 100%;" border="1">
+            <thead>
+                <tr>
+                    <th>IP</th>
+                    <th>Path</th>
+                    <th>Container</th>
+                    <th>Timestamp</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($requests as $request) : ?>
+                    <tr>
+                        <td><?= $request['ip'] ?></td>
+                        <td><?= $request['path'] ?></td>
+                        <td><?= $request['host'] ?></td>
+                        <td><?= $request['requested_at'] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </body>
+</html>
